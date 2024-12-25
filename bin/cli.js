9184cff667de5bfc6eb69ebcd4626e71c43de51a
ShinnosukeKomiya <komiya.shinnosuke@lmi.ne.jp>
2024-12-25 19:42:43 +0900

    refac

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,4 @@
 #!/usr/bin/env node
-
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import { main } from '../src/index.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 main();
