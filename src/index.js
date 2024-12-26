5ab0c9aa7f2c5005bb5d224c6dab281cc4e50c3d
ShinnosukeKomiya <komiya.shinnosuke@lmi.ne.jp>
2024-12-26 18:12:00 +0900

    fix route

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ export async function main() {
   try {
     // 必要なすべてのYAMLファイルを読み込む
     const yamlContents = {
-      basicInfos: await fs.readFile(path.join(process.cwd(), 'docs/requirements', 'basicInfos.yml'), 'utf8'),
-      requirementsList: await fs.readFile(path.join(process.cwd(), 'docs/requirements', 'requirementsList.yml'), 'utf8'),
-      nonFunctionsList: await fs.readFile(path.join(process.cwd(), 'docs/requirements', 'nonFunctionsList.yml'), 'utf8'),
-      functionsList: await fs.readFile(path.join(process.cwd(), 'docs/requirements', 'functionsList.yml'), 'utf8'),
-      tobeOperationFlow: await fs.readFile(path.join(process.cwd(), 'docs/requirements', 'tobeOperationFlow.yml'), 'utf8'),
-      screensList: await fs.readFile(path.join(process.cwd(), 'docs/requirements', 'screensList.yml'), 'utf8')
+      basicInfos: await fs.readFile(path.join(process.cwd(), 'docs', 'basicInfos.yml'), 'utf8'),
+      requirementsList: await fs.readFile(path.join(process.cwd(), 'docs', 'requirementsList.yml'), 'utf8'),
+      nonFunctionsList: await fs.readFile(path.join(process.cwd(), 'docs', 'nonFunctionsList.yml'), 'utf8'),
+      functionsList: await fs.readFile(path.join(process.cwd(), 'docs', 'functionsList.yml'), 'utf8'),
+      tobeOperationFlow: await fs.readFile(path.join(process.cwd(), 'docs', 'tobeOperationFlow.yml'), 'utf8'),
+      screensList: await fs.readFile(path.join(process.cwd(), 'docs', 'screensList.yml'), 'utf8')
     };
     // MarkdownGeneratorのインスタンス化
     const generator = new MarkdownGenerator(yamlContents);
